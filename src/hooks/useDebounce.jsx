32b79f017ebd538359d559311cb5b21bdc6a09c1
raymondo <rchukwu94@gmail.com>
2023-07-18 09:43:35 -0700

    minor fix

--- src/hooks/useDebounce.jsx
+++ src/hooks/useDebounce.jsx
@@ -5,12 +5,10 @@ const useDebounce = (value, delay = 500) => {
 
   useEffect(() => {
     const search = setTimeout(() => {
-      console.log("setting debounced value");
       setDebounceValue(value);
     }, delay);
 
     return () => {
-      console.log("removing debounced value");
       clearTimeout(search);
     };
   }, [value, delay]);
